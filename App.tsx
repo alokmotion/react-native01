b1984b81a7fea7a2a27e35f81bf69475f7e3a1ef
alokmotion <alokpatel406@gmail.com>
2025-02-13 02:01:59 +0530

    inline css

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { View, Text, Image, Button } from 'react-native'
+import { SafeAreaView, Text, Image, Button, Pressable } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      <Text>Ka ho babu Androide Developer Ban Rahe ho</Text>
+    <SafeAreaView style={{ width:"100%", height:"100%", backgroundColor:"black"  ,display:'flex', alignItems: "center" }}>
+      <Text style={{color:"white", marginTop:20, marginBottom:20}} >Ka ho babu Androide Developer Ban Rahe ho</Text>
       <Image 
       style={{width:200 , height:200}}
       source={{uri:'https://images.unsplash.com/photo-1739289354200-f5db511bd08b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D'}}/>
-    <Button title='Click Me'></Button>
-    </View>
+
+    {/* <Button title='Click Me'></Button> */}
+
+    <Pressable >
+      <Text style={{color:"white", marginTop:20, marginBottom:20}} >Click Me</Text>
+    </Pressable>
+    </SafeAreaView>
   )
 }
 
